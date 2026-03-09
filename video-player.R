d3a0e4b0e758328d4d531d5001d43874420a28e4
Gabriel Broussard Korr <mantis9877@gmail.com>
2026-03-09 00:20:57 -0400

    last commit before rewriting to remove the render.image downsampling; we can completely downsample using av

--- video-player.R
+++ video-player.R
@@ -3,12 +3,37 @@ library(jpeg)
 
 video_path = 'XcQ.mp4'
 
+#i HATE the look of this page with all the documentation here. Maybe write an Rmd instead? I'd really like to stress the simplicity of this stuff
+#maybe have a bare this.R and an RMD article explaining it
+
+#rmd format:
+# what the package does + demo
+# all the functions by name and  brief desc
+# section for each function w/ code and how it works and examples
 
 
-pal = function(str='▓∏░: ') strsplit(str,'')[[1]] #convenience function to generate palette from string
 
+#' Render Matrix to Console
+#' 
+#' Converts and prints a matrix as unicode characters specified by `palette`, clearing the previous frame.
+#' 
+#' The function adds newlines to the matrix, serializes it, and uses it to key `palette` to convert the integers into characters. This character string is then printed with `cat()`, overwriting any previous cat output with a carriage return (`\r`).
+#' 
+#' @param M Integer matrix with values corresponding to the character in `palette` to render. (indexed by 0)
+#' @param palette Vector of characters to render for values in `M` of 0, 1, 2, etc. Defaults to 1-bit black/white.
+#' @section Newlines:
+#' Several `\t`'s are used in place of the actual newline character `\n`. Due to how tab characters are rendered, this produces the same behavior as a newline while keeping the entire `cat()` output in a single string so that it can be overwritten by `\r`.
 render.matrix = function(M,palette=c('▓',' ')) cat('\r', c('\t',palette)[t(cbind(M,matrix(-1,nrow(M),10)))+2], sep='')
 
+#' Downscale and Convert Image for Rendering
+#' 
+#' Downsamples, grayscales, and quantizes an image to produce a renderable bitmap matrix. The resultant matrix has 2:1 detail resolution (i.e. twice as much resolution horizontally than vertical) to match how characters are rendered to the console, and it is composed of integers corresponding to the pixel's shade. 
+#' 
+#' Performs simple luma grayscaling and nearest-neighbor downsampling. Grayscale colors are quantized linearly to match the desired number of `shades`.
+#' 
+#' @param img RGB image array as produced by `jpeg::readJPEG()`.
+#' @param size Width to downscale image to.
+#' @param shades Shades of gray to quantize the image to. 2 = black/white, 3 = black/gray/white, etc.
 render.image = function(img,size=128,shades=2){ #size = horizontal resolution
 	by = ncol(img)/size
 	
@@ -24,6 +49,14 @@ render.image = function(img,size=128,shades=2){ #size = horizontal resolution
 	floor(img * shades)
 }
 
+#' Load Video File for Playing
+#'
+#' Downscales input video and extracts frames to be used in `play.video()`.
+#' 
+#' `base::readJPEG()` is costly at higher resolutions, so it's worth formally downscaling (i.e. with traditional video tools) the video before attempting to play it. Be sure not to downscale further than you intend to render.
+#'
+#' @param path Path to video file. I think this works for any video format.
+#' @param size Width to downscale video to. Doesn't have to be exact; anything below 400 will produce nice output video.
 load.video = function(path,size=128) { #converts to low-res 25fps
 	cat('Downscaling...\n')
 	info = av_media_info(path)$video
